91071014aea0b371dbf440fb19a1fb0edfb553ad
hlghpz <1090377416@qq.com>
2022-03-05 22:50:05 +0800

    🔧工具(Add component library): 使用naive-ui作为前端组件库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
+import { setupNaive } from '@/plugins'
 
+import './index.css'
+
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
 
 const app = createApp(App)
+
+// 注册全局常用的 naive-ui 组件
+setupNaive(app)
+
 app.use(router)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import stypleImport, { VantResolve } from 'vite-plugin-style-import'
 import path from "path"
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    stypleImport({
-      resolve: [VantResolve()],
-    }),
+    vue()
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "src"),
+      "components": path.resolve(__dirname, "src/components"),
+      "views": path.resolve(__dirname, "src/views"),
+      "assets": path.resolve(__dirname, "src/assets"),
     }
   }
 })
